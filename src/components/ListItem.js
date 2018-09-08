b6ceba4be63050c9c84d97a91e7085c18c7a4826
yelena guliyeva <gulbadam@gulbadam.com>
2018-09-07 20:34:14 -0700

    description

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,16 +4,26 @@ import { connect } from 'react-redux';
 import {CardSection} from './common';
 import * as actions from '../actions';
  class ListItem extends Component {
+   renderDescription() {
+     const {library, selectedLibraryId} =this.props;
+     if (library.item.id===selectedLibraryId) {
+       return (
+         <Text>{library.item.description}</Text>
+       )
+     }
+   }
   render() {
     const { id, title } = this.props.library.item;
     const {titleStyle} =styles;
     console.log(this.props)
+    console.log(this.selectedLibraryId);
     return (
       <TouchableWithoutFeedback onPress={()=> this.props.selectLibrary(id) }>
       <View>
     <CardSection>
                 <Text style={titleStyle}>{title}</Text>
                 </CardSection>
+                {this.renderDescription()}
                 </View>
                 </TouchableWithoutFeedback>
       
@@ -26,4 +36,9 @@ const styles ={
     paddingLeft: 15,
   }
 }
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return {
+    selectedLibraryId: state.selectedLibraryId
+  }
+}
+export default connect(mapStateToProps, actions)(ListItem);
